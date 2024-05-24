3218919dcc830098681f10d27992e25201479657
LizFujimori <liizfdaan@gmail.com>
2024-05-23 23:39:55 -0400

    commit
    ta dando erro em alguma coisa eu vou me matar

--- index.js
+++ index.js
@@ -3,6 +3,37 @@ const conn = require("./db/conn");
 
 const Usuario = require("./moldels/Usuario");
 
+const express = require("express");
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json());
+
+app.get("/usuarios/novo", (req, res) => {
+    res.sendFile(`${__dirname}/views/formUsuario.html`);
+});
+
+app.post("/usuarios/novo", async (req, res) => {
+    const dadosUsuario = {
+        nickname: req.body.nickname,
+        nome: req.body.nome,
+    };
+
+    const usuario = await Usuario.create(dadosUsuario);
+    res.send("Usuario inserido com o id: " + usuario.id)
+});
+
+app.listen(8000, () => {
+    console.log("rodandinho :P");
+});
+
+
+
+
 conn
     .sync()
     .authenticate()
@@ -10,7 +41,7 @@ conn
        console.log("conectado e sync ao db :P");
     })
     .catch((err) => {
-     console.log("erro :(" + err);
+     console.log("erro :( " + err);
     });
 
 
